891d67b43a61281ae1df53e3d890e11b5134061b
eal99 <ethan.leveille@uvm.edu>
2024-01-17 12:45:33 -0500

    Finished the manylinks piece

--- generate.py
+++ generate.py
@@ -8,18 +8,28 @@ x = 0
 y = 0
 z = height/2
 
-pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+for b in range(5):
 
-for j in range(10):
+    for j in range(5):
 
-    for i in range(10):
-        z += 1
-        length = length * .90
-        width = width * .90
-        height = height * .90
         pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
-    height, width, length = 1, 1, 1
-    z = height/2
-    x += 1
+
+        for i in range(10):
+            z += 1
+            length = length * .90
+            width = width * .90
+            height = height * .90
+            pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+
+        height, width, length = 1, 1, 1
+        z = height/2
+        x += 1
+    length = 1
+    width = 1
+    height = 1
+    x = 0
+    y += 1
+    z = height / 2
+
 
 pyrosim.End()
