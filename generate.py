4c3564579477111076b1f3fecf2ddddbcc7fe532
eal99 <ethan.leveille@uvm.edu>
2024-01-17 13:45:46 -0500

    Completed up to step 34

--- generate.py
+++ generate.py
@@ -8,4 +8,15 @@ def create_world(length, width, height, x, y, z):
     pyrosim.End()
 
 
-create_world(1, 1, 1, 0, 0, .5)
+def create_robot():
+
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Link0", pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0, 0, 1])
+    pyrosim.Send_Cube(name="Link1", pos=[0, 0, .5], size=[1, 1, 1])
+    pyrosim.End()
+
+
+create_world(1, 1, 1, -2, 2, .5)
+
+create_robot()
