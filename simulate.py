2d6986f587852caf57f54666dbcbbb23cd28aa6f
eal99 <ethan.leveille@uvm.edu>
2024-02-05 14:54:50 -0500

    Completed Assignment 4 up to step 20

--- simulate.py
+++ simulate.py
@@ -1,17 +1,28 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
-p.setGravity(0,0,-9.8)
+p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(10000)
+print(backLegSensorValues)
+exit()
+
 for i in range(1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    print(backLegTouch)
     time.sleep(1/60)
-    print(i)
+    # print(i)
+
+
 p.disconnect()
