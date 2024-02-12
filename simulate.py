6f013a7617e3d09cef2bf1b67e2080abeb16d4be
eal99 <ethan.leveille@uvm.edu>
2024-02-12 18:28:43 -0500

    Completed Assignment 4 up to step 27

--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
+import os
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -11,18 +12,19 @@ p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
-
 pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorValues = numpy.zeros(10000)
-print(backLegSensorValues)
-exit()
+backLegSensorValues = numpy.zeros(1000)
 
 for i in range(1000):
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    print(backLegTouch)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/60)
-    # print(i)
-
 
 p.disconnect()
+print(backLegSensorValues)
+subdirectory = 'data'
+
+if not os.path.exists(subdirectory):
+    os.makedirs(subdirectory)
+filename = os.path.join(subdirectory, 'backLegSensorValues.npy')
+numpy.save(filename, backLegSensorValues)
